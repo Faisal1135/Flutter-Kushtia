29591f29583ef5a3e4c7858ce67f918002138797
Faisal Kabir Galib <bear.galib.bg@gmail.com>
2021-09-07 09:57:56 +0600

    init datatype

---
+++ main.dart
@@ -0,0 +1,38 @@
+void main() {
+  // print("My name is" + " " + name + "My age is ");
+
+  print(
+      "My name is $name . My age is $age , is student - $isStudent.after 6 years my age will be ${age + 6}");
+  // print("hellow world");
+  print(students[1]);
+  print(name);
+  print("name");
+  print(studentsm[1]["name"]);
+}
+
+var name = "faisal Kabir Galib";
+
+// String name = "Faisal Kabir Galib";
+
+//  My name is gALIB, MY age is 24;
+
+int age = 24;
+// double ageStr = "24";
+
+double agedoub = 24.586;
+
+bool isStudent = false;
+
+List students = ["Irfan-24-kustia-038484", "Sajjad", "Ananta", 34];
+
+Map<String, dynamic> studet1 = {
+  "name": "Faisal Kabir",
+  "age": 24,
+  "isStudent": true
+};
+
+List<Map<String, dynamic>> studentsm = [
+  {"name": "Irfan", "age": 23, "address": "bvkjfnn"},
+  {"name": "Sajjad", "age": 24, "address": "djff"},
+  {"name": "Antanta", "age": 25, "address": "Osomvokari"},
+];
