48f692f391af5fc19cf8ce12188b2654a1546fc7
Faisal Kabir Galib <bear.galib.bg@gmail.com>
2021-09-11 10:50:50 +0600

    function init

--- main.dart
+++ main.dart
@@ -1,78 +1,98 @@
 void main() {
-  // password = "nofkm";
-  // if (age > 0 && age <= 15) {
-  //   print("You are Child");
+  print("I am befor");
 
-  //   // } else if (age < 25) {
-  //   //   print("YOU ARE A tEEN");
-  // } else if (age > 15 && age <= 25) {
-  //   print("YOu are a teen");
-  // } else if (age > 25 && age <= 40) {
-  //   print("You are an adult");
-  // } else {
-  //   print("YOU ARE OLD");
-  // }
-
-  if (inputPass == passwordRidoy) {
-    print("Hridoy vai");
-  } else if (inputPass == passwordIrfan) {
-    print("Irfan vai");
-  } else if (inputPass == "ybf") {
-    print("omuk vai");
-  } else if (inputPass == "yfjj") {
-    print("tomuk vai");
+  age = 30;
+  if (age > 0 && age <= 15) {
+    print("You are a child");
+  } else if (age <= 25) {
+    print("You are a Teen");
+  } else if (age == 40) {
+    print("You are adult");
   } else {
-    print("Sajjad vai");
+    print("You are an old"); // Default
   }
 
-  switch (inputPass) {
-    case "abc":
-      print("Hridoy vai");
-      break;
+  // Function
 
-    case "def":
-      print("Irfan Vai");
-      break;
-    case "ghi":
-      print("Sajjad  Vai");
-      break;
-    default:
-      print("Kothao kew nai");
+  // t Fn (input // amm, chinni , pani ) {  return }
+
+  doBazar(bool hasPani, {int bagnum = 1}) {
+    print("flat er niche namo ");
+    print("peyarotola bazar e zau  ");
+    print("rickshaw niye NS road zaw . ");
+    print("Collect bazarer bag $bagnum. ");
+    print("rickshaw niye pT road zaw . ");
+
+    if (hasPani) {
+      print("Take water");
+    }
+    print("flat 4th floor ");
   }
 
-  // var result = age > 15 ? "YOU are a child" : "You are an adult";
+  // Day 01
 
-  // switch case
-// bool condition ? in case ture : in case false;
-  // if (bool ){
-  //
-  //
-  // }else{
-  // }
+  print(doBazar(true));
+
+  // print("flat er niche namo ");
+  // print("peyarotola bazar e zau  ");
+  // print("rickshaw niye NS road zaw . ");
+  // print("Collect bazarer bag . ");
+  // print("rickshaw niye pT road zaw .");
+  // print("flat 4th floor ");
+
+// Day 02
+
+  print(doBazar(true));
+
+  // print("flat er niche namo ");
+  // print("peyarotola bazar e zau  ");
+  // print("rickshaw niye NS road zaw . ");
+  // print("Collect bazarer bag . ");
+  // print("rickshaw niye pT road zaw . ");
+  // print("flat 4th floor ");
+
+// Day 03
+
+  print(doBazar(false, bagnum: 2));
+
+  // print("flat er niche namo ");
+  // print("peyarotola bazar e zau  ");
+  // print("rickshaw niye NS road zaw . ");
+  // print("Collect bazarer bag 2 ta . ");
+  // print("rickshaw niye pT road zaw . ");
+  // print("flat 4th floor ");
+
+  // bool state ? in case True : in case false;
 
   // if (age > 15) {
-  //   print("YOu are an adult");
+  //   print("You are a Adult");
   // }
+
+  // print("You are a Teen");
+  // print("You are a Adult");
+  // print("You are a old");
 }
-
-// t fn (input){
-// body
-
-// return output
-//
-// }
-
-var inputPass = "abc";
-
-var passwordRidoy = "abc";
-var passwordIrfan = "def";
-var passwordSajjad = "ghi";
 
 int age = 15;
 
-var name = "Galib";
+// t Fn (input){ body }
+// Braching , Conditional , Block
 
-// 15 - Child
-// 15-25 = Teen
-// 25-40 = Adult
-// 40 up - Oldprint("You are Child")
+// 15 > - Child,
+// 15 -25 - Teen,
+// 25 - 40 - Adult
+// 40 up - old
+
+// ~ - tilda
+// `` - backtik
+// "!" - bang
+// @ - at the rate
+// # - hash, shernap, octathod
+// % - percent
+// ^ - carrot/cap
+// & - ampersand
+// * - Asteris
+// (parentheis / round braces){curly braces }[ squre braces]
+// \- backward slash , / -forward slash , <angel braces>
+
+// \home\galib\
